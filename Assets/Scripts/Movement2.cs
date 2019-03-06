3a573b024e6edc6db3d74b3f2a80765be911ebb9
AurSka <aur.skarbalius@gmail.com>
2019-03-06 15:35:34 +0200

    All the progress

---
+++ Assets/Scripts/Movement2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class Movement2 : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Turn();
+    }
+
+    void Move()
+    {
+        animator.SetFloat("Forward", Input.GetAxis("Vertical"));
+    }
+
+    void Turn()
+    {
+        animator.SetFloat("Turn", Input.GetAxis("Horizontal"));
+    }
+}
