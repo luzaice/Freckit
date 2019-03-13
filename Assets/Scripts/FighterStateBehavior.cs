4e2eac3010891a916fc5fe5b12421c9557ffbebb
AurSka <aur.skarbalius@gmail.com>
2019-03-13 13:52:28 +0200

    Atnaujinimas

---
+++ Assets/Scripts/FighterStateBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterStateBehavior : StateMachineBehaviour
+{
+    public float horizontalForce;
+    public float verticalForce;
+
+    public FighterStates behaviorState;
+
+    protected Fighter fighter;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (fighter == null)
+        {
+            fighter = animator.gameObject.GetComponent<Fighter>();
+        }
+
+        fighter.currentState = behaviorState;
+
+        fighter.body.AddRelativeForce(new Vector3(0, verticalForce, 0));
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        fighter.body.AddRelativeForce(new Vector3(0, 0, horizontalForce));
+    }
+}
